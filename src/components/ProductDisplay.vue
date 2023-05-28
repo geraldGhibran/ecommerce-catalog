44902fb77a7aeff7e3fceb77ce07d620de6970ae
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-05-28 19:40:00 +0700

    .

--- src/components/ProductDisplay.vue
+++ src/components/ProductDisplay.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="product_display">
-    <div class="product_card-avaible">
-      <div class="product_card-right">
-        <img :src="product?.data?.image" alt="" />
+    <div class="product_card">
+      <div
+        class="product_card-avaible"
+        :class="isProductAvailable ? 'display-none' : 'product_card-avaible'"
+      >
+        <div class="product_card-right">
+          <img :src="product?.data?.image" class="product_img" />
+        </div>
+
+        <div class="product_card-left">
+          <h1>{{ product?.data?.title }}</h1>
+        </div>
       </div>
     </div>
   </div>
@@ -15,28 +24,46 @@ export default {
   data() {
     return {
       index: 0,
-      product: {}
+      product: {},
+      isProductAvailable: false
     }
   },
 
   methods: {
     async API() {
       try {
-        const response = await fetch(`https://fakestoreapi.com/products/${this.index}`)
-        console.log(response)
+        const url = `https://fakestoreapi.com/products/${this.index}`
+        const response = await fetch(url)
         if (!response.ok) {
           throw new Error('Request Failed')
         }
         const result = await response.json()
-        this.product_display = result
+        return result
       } catch (error) {
         this.error = error.message
+      }
+    },
+
+    async singleProduct() {
+      if (this.index <= 20) {
+        this.index++
+      } else {
+        this.index = 1
+      }
+
+      const data = await this.API()
+      console.log(data)
+      if (data.category === "men's clothing" || data.category === "women's clothing") {
+        this.product = { data }
+        this.isProductAvailable = true
+      } else {
+        this.isProductAvailable = false
       }
     }
   },
 
   mounted() {
-    this.API()
+    this.singleProduct()
   }
 }
 </script>
