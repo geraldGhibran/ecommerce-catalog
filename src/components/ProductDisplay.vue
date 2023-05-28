6f96593389f0772e23f34fbc690d72ec3ab98da9
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-05-28 18:15:56 +0700

    .

---
+++ src/components/ProductDisplay.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="product_display">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductDisplayComponent'
+}
+</script>
+
+<style lang="scss" scoped></style>
