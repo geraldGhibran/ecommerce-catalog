904acfbf0b19827d93972b7160a68ccd2aaecaeb
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-05-28 18:56:50 +0700

    .

--- src/components/ProductDisplay.vue
+++ src/components/ProductDisplay.vue
@@ -1,15 +1,43 @@
 <template>
   <div class="product_display">
-
-    <div class="product_card">
-        
+    <div class="product_card-avaible">
+      <div class="product_card-right">
+        <img :src="product?.data?.image" alt="" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'ProductDisplayComponent'
+  name: 'ProductDisplayComponent',
+
+  data() {
+    return {
+      index: 0,
+      product: {}
+    }
+  },
+
+  methods: {
+    async API() {
+      try {
+        const response = await fetch(`https://fakestoreapi.com/products/${this.index}`)
+        console.log(response)
+        if (!response.ok) {
+          throw new Error('Request Failed')
+        }
+        const result = await response.json()
+        this.product_display = result
+      } catch (error) {
+        this.error = error.message
+      }
+    }
+  },
+
+  mounted() {
+    this.API()
+  }
 }
 </script>
 
