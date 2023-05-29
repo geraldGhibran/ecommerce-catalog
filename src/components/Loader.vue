38dcf214d59949fd9929f318b60b403837c0d46a
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-05-29 09:29:44 +0700

    .

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -23,8 +23,8 @@ export default {
 }
 
 .loader {
-  border: 16px solid #f3f3f3; /* Warna loader */
-  border-top: 16px solid #3498db; /* Warna loader */
+  border: 16px solid #f3f3f3;
+  border-top: 16px solid #3498db;
   border-radius: 50%;
   width: 120px;
   height: 120px;
